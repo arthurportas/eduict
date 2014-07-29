ac97d46a0e7bde0d1eda43f48aa43bbb5e3e77c3
arthurportas <arthur.m@iol.pt>
2014-07-29 22:52:07 +0000

    arquillian tests fix to run on openshift

--- src/main/java/com/eduict/controller/MemberRegistration.java
+++ src/main/java/com/eduict/controller/MemberRegistration.java
@@ -47,7 +47,6 @@ public class MemberRegistration {
    public void register() throws Exception {
       log.info("Registering " + newMember.getName());
       em.persist(newMember);
-      facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Registered!", "Registration successful"));
       memberEventSrc.fire(newMember);
       initNewMember();
    }
