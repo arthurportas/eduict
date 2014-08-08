f92a951f02434bc636caf356abafa11d718034f9
arthurportas <arthur.m@iol.pt>
2014-08-08 10:54:15 +0000

    fix serialization issues

--- src/main/java/com/eduict/model/Domain.java
+++ src/main/java/com/eduict/model/Domain.java
@@ -7,8 +7,11 @@ import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
-
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import java.util.List;
 
 import org.hibernate.validator.constraints.Email;
@@ -16,6 +19,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = "description"))
 public class Domain implements Serializable {
 
@@ -24,15 +28,18 @@ public class Domain implements Serializable {
    @Id
    @GeneratedValue
    @Column(name="DOMAIN_ID")
+   @XmlAttribute
    private Long id;
 
    @NotNull
    @Size(min = 1, max = 100)
    @Pattern(regexp = "[A-Za-z ]*", message = "domain description must contain only letters and spaces")
    @Column(name="DESCRIPTION")
+   @XmlAttribute
    private String description;
 
    @OneToMany(fetch = FetchType.EAGER, mappedBy = "domain", cascade=CascadeType.ALL)
+   @XmlTransient
    private List<Question> questions;
    
    public Long getId() {
