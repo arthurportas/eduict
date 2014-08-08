bc41f85a02af958124edcc314a780980648963eb
arthurportas <arthur.m@iol.pt>
2014-08-08 09:31:08 +0000

    Domain dataset

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,10 @@
 -- You can use this file to load seed data into the database using SQL statements
-insert into Member (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212') 
+insert into Member (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212');
+
+INSERT INTO Domain (id, description) VALUES (1, 'Compreensão das TIC na educação');
+INSERT INTO Domain (id, description) VALUES (2, 'Currículo e Avaliação');
+INSERT INTO Domain (id, description) VALUES (3, 'Pedagogia');
+INSERT INTO Domain (id, description) VALUES (4, 'TIC');
+INSERT INTO Domain (id, description) VALUES (5, 'Organização e Administração');
+INSERT INTO Domain (id, description) VALUES (6, 'Desenvolvimento Profissional Docente');
+
