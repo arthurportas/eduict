3b1c32399de0619f133be30c7b4f25647c6bb67b
arthurportas <arthur.m@iol.pt>
2014-08-10 15:43:34 +0000

    login servlet debug

--- src/main/java/com/eduict/servlet/async/LoginTask.java
+++ src/main/java/com/eduict/servlet/async/LoginTask.java
@@ -1,17 +1,12 @@
 package com.eduict.servlet.async;
 
 import java.util.concurrent.Callable;
-import java.util.logging.Logger;
-import javax.inject.Inject;
+
 
 public class LoginTask implements Callable<String> {
 
-    @Inject
-    private Logger log;
-  
     @Override
     public String call() throws Exception {
-        log.info("LoginTask call()");
         return "logintask";
     }
 }
