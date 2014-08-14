9957cb4d295b48148d175e9cded8d5c8a24c4dfa
arthurportas <arthur.m@iol.pt>
2014-08-14 08:44:01 +0000

    Fix import

--- src/main/java/com/eduict/model/School.java
+++ src/main/java/com/eduict/model/School.java
@@ -10,7 +10,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
-
+import com.eduict.model.Region;
 
 
 @Entity
