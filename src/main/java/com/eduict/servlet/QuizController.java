59e8c214fefd96868fdeb4f4fbf4255d8b42f915
arthurportas <arthur.m@iol.pt>
2014-10-06 22:38:47 +0000

    with login, create a new quiz on demand

--- src/main/java/com/eduict/servlet/QuizController.java
+++ src/main/java/com/eduict/servlet/QuizController.java
@@ -48,7 +48,7 @@ public class QuizController extends HttpServlet {
             User user =  (User) session.getAttribute("user");
             if(user != null) {
                 Quiz newQuiz = new Quiz();
-                newQuiz.setLevels = levelListProducer.levels();//fetch levels list to associate with this new quiz
+                //newQuiz.setLevels = levelListProducer.levels();//fetch levels list to associate with this new quiz
                 user.getQuizzes().add(newQuiz);
                 newQuiz.setUser(user);
                 quizRegistration.registerNewQuiz(newQuiz);
