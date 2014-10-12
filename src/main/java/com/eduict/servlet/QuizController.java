3109afedcb96bc6361c9fcbd3f081c902ac3555d
Arthur Portas <arthurportas@gmail.com>
2014-10-12 22:07:55 +0100

    if User is found in session, on /quiz, if previousQuizId found on cookie, recover previous created quiz

--- src/main/java/com/eduict/servlet/QuizController.java
+++ src/main/java/com/eduict/servlet/QuizController.java
@@ -9,10 +9,7 @@ import javax.inject.Inject;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -42,31 +39,49 @@ public class QuizController extends HttpServlet {
         StringBuilder errorMessage = new StringBuilder();
 
         try {
-            //if user has session, then create a new Quiz for him,
+            //if user has session, and no cookie value then create a new Quiz for him,
+            //if user has session and cookie value, load quiz upon cookie reference
             //and fetch user quiz history
             HttpSession session = request.getSession();
             
             if (session.getAttribute("user") != null) {
                 User user =  (User) session.getAttribute("user");
                 log.info("session found for user presenting a new quiz");
-                Quiz newQuiz = new Quiz();
-                newQuiz.setLevels(levelListProducer.levels());//fetch levels list to associate with this new quiz
-                newQuiz.setUser(user);
-                List<Quiz> userQuizzes = new ArrayList<Quiz>();
-                if (user.getQuizzes().isEmpty()) {
-                    userQuizzes.add(newQuiz);
-                } else {
-                    for (Quiz q : user.getQuizzes()) {
-                        userQuizzes.add(q);
+
+                //read cookies to see if exists previously created quiz
+                Cookie[] cookies = request.getCookies();
+                Long previousId = null;
+                for (Cookie obj : cookies) {
+                    if (obj.getName().equals("previousId")) {
+                        previousId = Long.parseLong(obj.getValue());
+                        Quiz previousQuiz = registrationService.lookupQuizById(previousId);
+                        previousQuiz.setLevels(levelListProducer.levels());
+                        request.setAttribute("quiz", previousQuiz);
+                        log.info("quiz: " + previousQuiz.toString());
+                        break;
                     }
-                    userQuizzes.add(newQuiz);
                 }
-                user.setQuizzes(userQuizzes);
-                quizRegistration.registerNewQuiz(newQuiz);
-
-                request.setAttribute("quiz", newQuiz);
-                log.info("quiz: " + newQuiz.toString());
-
+                if (previousId == null) {
+                    Quiz newQuiz = new Quiz();
+                    newQuiz.setLevels(levelListProducer.levels());//fetch levels list to associate with this new quiz
+                    newQuiz.setUser(user);
+                    List<Quiz> userQuizzes = new ArrayList<Quiz>();
+                    if (user.getQuizzes().isEmpty()) {
+                        userQuizzes.add(newQuiz);
+                    } else {
+                        for (Quiz q : user.getQuizzes()) {
+                            userQuizzes.add(q);
+                        }
+                        userQuizzes.add(newQuiz);
+                    }
+                    user.setQuizzes(userQuizzes);
+                    quizRegistration.registerNewQuiz(newQuiz);
+                    Cookie cookie = new Cookie("previousId", newQuiz.getId().toString());
+                    cookie.setMaxAge(30 * 60); //1/2 hour
+                    response.addCookie(cookie);
+                    request.setAttribute("quiz", newQuiz);
+                    log.info("quiz: " + newQuiz.toString());
+                }
             } else {
                 log.info("no session found presenting a demo quiz");
                  //else present a demo quiz
